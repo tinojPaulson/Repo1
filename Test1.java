9ec0ddd0e7a4c64da97e8a3f7c3343b51a627e7c
tinojPaulson <tinojcs0123@gmail.com>
2020-02-23 17:47:58 +0000

    added

--- Test1.java
+++ Test1.java
@@ -2,6 +2,6 @@ public class Test1{
 
 	public static void main(String[] arg){
 		for(int i=0; i<10; i++)
-			System.out.println("This is test loop.. " + i);
+			System.out.println("===This is test loop.. " + i);
 	}
 }
